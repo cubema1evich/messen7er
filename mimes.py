473fe001c09e9ecec5adf3a9c10c39b89f245efd
cubema1evich <cubemalevich1@gmail.com>
2025-04-04 21:33:30 +0300

    Добавлены новые типы для работы с файлами

--- mimes.py
+++ mimes.py
@@ -1,7 +1,15 @@
 mimes = {
     '.html': 'text/html',
     '.css': 'text/css',
-    '.js': 'text/javascript'
+    '.js': 'text/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.gif': 'image/gif',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword',
+    '.docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    '.txt': 'text/plain'
 }
 
 def get_mime(file_name):
