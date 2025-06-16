de93b2ee0ebd959518486a856d23665864fe32c3
Intelligent-95 <maxim.zaboloczckij@yandex.ru>
2025-06-17 00:07:04 +0300

    убрал модуль за ненадобностью

--- views/crypto.py
+++
@@ -1,6 +0,0 @@
-from .base import View, Response
-
-class PublicKeyView(View):
-    def get(self, request):
-        with open('public_key.pem', 'rb') as f:
-            return Response(f.read(), content_type='application/x-pem-file')
